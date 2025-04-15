4ce697e3961b7444b927d928f4b667e3c756d364
Mikulas Machovec <mikimachovec@gmail.com>
2025-04-15 15:00:58 +0000

    mostly done

--- routes/api.js
+++ routes/api.js
@@ -1,51 +1,42 @@
 'use strict';
-
-const { Board } = require('../model/msg-model');
-
 const BoardModel = require('../model/msg-model').Board;
 const ThreadModel = require('../model/msg-model').Thread;
 const ReplyModel = require('../model/msg-model').Reply;
 
-
-
+let date = new Date()
 module.exports = function (app) {
   
   app
    .route('/api/threads/:board')
    .post(async (req,res) => {
       const {text, delete_password} = req.body
-      let board = req.body.board
-      if(!board){
-         board = req.params.board
-      }
+      let board = req.params.board || req.body.board
+      
       const newThread = new ThreadModel({
          text: text,
-         delete_password: delete_password,
-         replies: []
+         delete_password: delete_password
       })
-      console.log('newThread', newThread)
+      //console.log('newThread', newThread)
       const foundBoard = await BoardModel.findOne({ name: board})
          if(!foundBoard){
             const newBoardModel = new BoardModel({
                name     : board,
                threads  : [newThread]
             })
-            console.log('newBoardModel', newBoardModel)
+            //console.log('newBoardModel', newBoardModel)
             const savedNewBoard = await newBoardModel.save()
-            console.log('newBoardData', savedNewBoard)
+            //console.log('newBoardData', savedNewBoard)
             if(!savedNewBoard){
-               console.log(err)
-               res.send('error','error with saving to database')
+               res.status(400).send('error with saving to database')
             } else{
                res.redirect('/b/' + board)
             }   
          } else {
             foundBoard.threads.push(newThread)
             const savedFoundBoard = await foundBoard.save()
-               console.log('savedFoundBoard', savedFoundBoard)
+               //console.log('savedFoundBoard', savedFoundBoard)
                if(!savedFoundBoard){
-                  console.log(err)
-                  res.send('error','error with saving to database')
+                  res.status(400).send('error with saving to database')
                } else{
                   res.redirect('/b/' + board)
                }    
@@ -58,7 +49,7 @@ module.exports = function (app) {
       
       if(!foundBoard){
          console.log('board not found')
-         res.json({error: 'Board has not been found'})
+         res.status(400).send('Board has not been found')
       } else {
          const threads = foundBoard.threads.map((thread)=>{
             const{
@@ -82,26 +73,29 @@ module.exports = function (app) {
             }
             
          })
-      res.json(threads)
+         let slicedThreads = threads.slice(0,10)
+      res.json(slicedThreads)
       }
 
    })
 
    .put(async (req,res) => {
-      const {report_id} = req.body
+      const {thread_id} = req.body
       let board = req.params.board
-      
-      const Board = await BoardModel.findOne({name: board})
-      if(!Board){
-         res.send('error', 'board not found')
-      } else {
-         let date = new Date()
-         let reportedThred = Board.threads.id(report_id);
-         reportedThred.reported = true;
-         reportedThred.bumped_on = date;
-         await Board.save()
-         res.send('reported')
-      } 
+      try{
+         const Board = await BoardModel.findOne({name: board})
+         if(!Board){
+            res.send('board not found')
+         } else {
+            let reportedThread = Board.threads.id(thread_id);
+            reportedThread.reported = true;
+            reportedThread.bumped_on = date;
+            await Board.save()
+            res.send('reported')
+         } 
+      } catch (error){
+         console.log(error.message)
+      }
    })
 
    .delete(async (req,res)=>{
@@ -109,24 +103,96 @@ module.exports = function (app) {
       let board = req.params.board
       const foundBoard = await BoardModel.findOne({name:board})
       if(!foundBoard){
-         res.send('error', 'board not found')
+         res.send('board not found')
       } else {   
       let threadForDel = foundBoard.threads.id(thread_id)
       if(threadForDel.delete_password === delete_password){
             foundBoard.threads.pull(threadForDel)
             await foundBoard.save()
-            res.json('success')
+            res.send('success')
          } else {
             res.send('incorrect password')
          }
       }
-   })
-
+   }
+   )
 
   app
    .route('/api/replies/:board')
-   .post(async(req,res)=>{})
-   .get(async(req,res)=>{})
-   .put(async(req,res)=>{})
-   .delete(async(req,res)=>{})
+   .post(async(req,res)=>{
+      const {thread_id, text, delete_password} = req.body
+      const board = req.params.board
+      const NewReplay = new ReplyModel({
+         text: text,
+         delete_password: delete_password
+      })
+      const Reply = await BoardModel.findOne({ name: board })
+      if(!Reply){
+         res.json('Board not found')
+      }else{
+         
+      const date =new Date((new Date().toLocaleString('en', {timeZone: 'Europe/Madrid'}))
+)
+         console.log(date)
+         let threadToAddReply = Reply.threads.id(thread_id)
+         threadToAddReply.bumped_on = date;
+         threadToAddReply.replies.push(NewReplay)
+         let SavedBoard  = await Reply.save()
+
+         res.redirect('/b/' + board)
+      }
+
+   })
+   .get(async(req,res)=>{
+      const { thread_id } = req.query
+      const board = req.params.board
+      const Board = await BoardModel.findOne({ name: board })
+      if(!Board){
+         res.json('Board not found')
+      }else{
+         const thread = Board.threads.id(thread_id)
+         res.json(thread)
+      }
+
+   })
+   .put(async(req,res)=>{
+      const {thread_id, reply_id} = req.body
+      const board = req.params.board
+      try{
+         const Board = await BoardModel.findOne({name: board})
+         if(!Board){
+            res.send( 'Board not found')
+         }else{
+            let date = new Date()
+            let thread = Board.threads.id(thread_id)
+            let reply = thread.replies.id(reply_id)
+            reply.reported = true;
+            reply.bumped_on = date;
+            await Board.save()
+
+            res.send('reported')
+         }
+      } catch(error){
+         console.log(error.message)
+      }
+   })
+   .delete(async(req,res)=>{
+      const board = req.params.board
+      const {thread_id, reply_id, delete_password} = req.body
+      try {
+         const Board = await BoardModel.findOne({name: board})
+         let thread = Board.threads.id(thread_id)
+         let reply = thread.replies.id(reply_id)
+         if (reply.delete_password === delete_password){
+            reply.text = '[deleted]'
+            await Board.save()
+            res.send('success')
+         }else{
+            res.send('incorrect password')
+         }
+      } catch (error) {
+         console.log(error.message)
+      }
+
+   })
 };
