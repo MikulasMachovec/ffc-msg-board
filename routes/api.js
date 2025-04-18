f10225da400d464addf770a8c15ed88380722b02
Mikulas Machovec <mikimachovec@gmail.com>
2025-04-18 12:29:36 +0200

    code part done

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,8 @@
 'use strict';
+
+const { text } = require('body-parser');
+const { Board } = require('../model/msg-model');
+
 const BoardModel = require('../model/msg-model').Board;
 const ThreadModel = require('../model/msg-model').Thread;
 const ReplyModel = require('../model/msg-model').Reply;
@@ -9,38 +13,43 @@ module.exports = function (app) {
   app
    .route('/api/threads/:board')
    .post(async (req,res) => {
+      try {
       const {text, delete_password} = req.body
       let board = req.params.board || req.body.board
       
+      let date = new Date();
+      // Create and save new thread first   
       const newThread = new ThreadModel({
-         text: text,
-         delete_password: delete_password
-      })
-      //console.log('newThread', newThread)
+         board,
+         text,
+         delete_password,
+         created_on: date,
+         bumped_on: date
+      });
+      // save thread
+      const savedThread = await newThread.save()
+      //Look for board
       const foundBoard = await BoardModel.findOne({ name: board})
-         if(!foundBoard){
-            const newBoardModel = new BoardModel({
-               name     : board,
-               threads  : [newThread]
-            })
-            //console.log('newBoardModel', newBoardModel)
-            const savedNewBoard = await newBoardModel.save()
-            //console.log('newBoardData', savedNewBoard)
-            if(!savedNewBoard){
-               res.status(400).send('error with saving to database')
-            } else{
-               res.redirect('/b/' + board)
-            }   
-         } else {
-            foundBoard.threads.push(newThread)
-            const savedFoundBoard = await foundBoard.save()
-               //console.log('savedFoundBoard', savedFoundBoard)
-               if(!savedFoundBoard){
-                  res.status(400).send('error with saving to database')
-               } else{
-                  res.redirect('/b/' + board)
-               }    
-         }
+
+      if (!foundBoard) {
+         // Create a new board if it doesn't exist
+         const newBoard = new BoardModel({
+           name: board,
+           threads: [savedThread]
+         });
+         await newBoard.save();
+       } else {
+         // Push the new thread into the board's thread array
+         foundBoard.threads.push(savedThread);
+         await foundBoard.save();
+       }
+   
+       // Redirect or send confirmation
+       res.redirect(`/b/${board}`);
+      } catch (error) {
+         console.error('Error creating thread:', error);
+         res.status(500).send('Server error');
+      }
    })
 
    .get(async(req,res)=>{
@@ -49,34 +58,35 @@ module.exports = function (app) {
       
       if(!foundBoard){
          console.log('board not found')
-         res.status(400).send('Board has not been found')
-      } else {
-         const threads = foundBoard.threads.map((thread)=>{
-            const{
-               _id,
-               text,
-               created_on,
-               bumped_on,
-               reported,
-               delete_password,
-               replies
-            } = thread
-            return{
-               _id,
-               text,
-               created_on,
-               bumped_on,
-               reported,
-               delete_password,
-               replies,
-               replycount: thread.replies.length
-            }
-            
-         })
-         let slicedThreads = threads.slice(0,10)
-      res.json(slicedThreads)
+         return res.status(400).send('Board has not been found')
       }
-
+      const sortedThreads = foundBoard.threads
+         .sort((a,b) => b.bumped_on - a.bumped_on)
+         .slice(0,10)
+         .map((thread)=>{
+            const sortedReplies = thread.replies
+               .sort((a,b)=> b.created_on - a.created_on)
+               .slice(0, 3)
+               .reverse()
+               .map(reply => ({
+                  _id: reply._id,
+                  text: reply.text,
+                  created_on: reply.created_on
+               }))       
+            
+            return{
+                  _id: thread._id,
+                  text: thread.text,
+                  created_on: thread.created_on,
+                  bumped_on: thread.bumped_on,
+                  replies: sortedReplies,
+                  replycount: thread.replies.length
+               }
+         
+      })
+         
+      res.json(sortedThreads)
+      
    })
 
    .put(async (req,res) => {
@@ -120,40 +130,69 @@ module.exports = function (app) {
   app
    .route('/api/replies/:board')
    .post(async(req,res)=>{
-      const {thread_id, text, delete_password} = req.body
-      const board = req.params.board
-      const NewReplay = new ReplyModel({
-         text: text,
-         delete_password: delete_password
-      })
-      const Reply = await BoardModel.findOne({ name: board })
-      if(!Reply){
-         res.json('Board not found')
-      }else{
+      try{
+         const {thread_id, text, delete_password} = req.body
+         const board = req.params.board
+         let date = new Date();
+
+         const NewReplay = new ReplyModel({
+            text,
+            delete_password,
+            created_on: date,
+            bumped_on: date
+         });
+
+         const Reply = await BoardModel.findOne({ name: board })
+
+         if(!Reply){
+            return res.json('Board not found')
+         }
+               // thread board id 68010e79f5128966321fbdc8
          
-      const date =new Date((new Date().toLocaleString('en', {timeZone: 'Europe/Madrid'}))
-)
-         console.log(date)
          let threadToAddReply = Reply.threads.id(thread_id)
+         
+         if(!threadToAddReply){
+            return res.json('Thread not found')
+         }
+
          threadToAddReply.bumped_on = date;
          threadToAddReply.replies.push(NewReplay)
-         let SavedBoard  = await Reply.save()
+         await Reply.save()
 
          res.redirect('/b/' + board)
-      }
-
+            
+      } catch (error){
+         console.error('Error posting reply:', error);
+         res.status(500).send('Server error');
+      }         
    })
-   .get(async(req,res)=>{
+   .get(async(req,res)=>{   
       const { thread_id } = req.query
       const board = req.params.board
       const Board = await BoardModel.findOne({ name: board })
       if(!Board){
-         res.json('Board not found')
-      }else{
-         const thread = Board.threads.id(thread_id)
-         res.json(thread)
+         return res.json('Board not found')
       }
 
+      const thread = Board.threads.id(thread_id)
+      if(!thread){
+         return res.json('Thread not found')
+      }
+      
+      const sanitizedThread ={
+         _id: thread.id,
+         text: thread.text,
+         created_on: thread.created_on,
+         bumped_on: thread.bumped_on,
+         replies: thread.replies.map((reply =>({
+            _id:reply._id,
+            text: reply.text,
+            created_on: reply.created_on
+         })))
+      } 
+
+
+      res.json(sanitizedThread)
    })
    .put(async(req,res)=>{
       const {thread_id, reply_id} = req.body
